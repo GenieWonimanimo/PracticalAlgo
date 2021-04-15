94f189c8b9134f489d1765abcabc007858d6b2ba
jiweon <kor98won@gmail.com>
2021-04-15 17:16:10 +0900

    실전 알고리즘_큐

---
+++ Queue.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MX = 1000005;
+int dat[MX];
+int head = 0, tail = 0; // head는 가장 앞에 있는 원소의 인덱스, tail은 가장 뒤에 있는 원소의 인덱스 + 1
+
+void push(int x) {
+    dat[tail++] = x;
+}
+
+void pop() {
+    head++;
+}
+
+int front() {
+    return dat[head];
+}
+
+int back() {
+    return dat[tail - 1];
+}
+
+bool isEmpty() {
+    return head == tail;
+}
+
+void test() {
+    push(1);
+    push(2);
+    push(3);
+    while (!isEmpty()) {
+        cout << front() << " ";
+        pop();
+    }
+}
+
+int main(void) {
+    test();
+    return 0;
+}
